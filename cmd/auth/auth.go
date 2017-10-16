076bdc1b2214a563dbdd93087d0ce01a580e6431
Vincent Schoener <vincent.schoener@gmail.com>
2017-10-16 14:48:57 +0200

    Add Auth App authentication to retrieve App access token

---
+++ cmd/auth/auth.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/wonderstream/twitch/core"
+	"github.com/wonderstream/twitch/credential"
+	"github.com/wonderstream/twitch/logger"
+	"github.com/wonderstream/twitch/storage"
+	"github.com/wonderstream/twitch/storage/repository"
+	"github.com/wonderstream/twitch/storage/transformer"
+)
+
+func main() {
+	credential := credential.NewCredential(credential.YAMLLoader{}, "./parameters.yml")
+	credential.LoadSetting()
+
+	l := logger.NewLogger()
+	l.Connect(credential.LoggerSettings)
+	defer l.(*logger.LogEntry).Client.Close()
+
+	database := storage.NewDatabase()
+	database.Logger = l
+	database.Connect(credential.GetDB())
+	defer database.DB.Close()
+
+	oauth2 := core.NewOAuth2(credential.GetTwitch())
+	oauth2.Logger = l
+	token, err := oauth2.RequestAppAccessToken()
+
+	if err != nil {
+		l.LogInterface(err)
+		return
+	}
+
+	r := repository.CredentialRepository{
+		Repository: repository.NewRepository(database, l),
+	}
+	sToken := transformer.TransformCoreTokenResponseToStorageCredential(token)
+	sToken.AppName = oauth2.AppName
+	r.SaveAppCredential(sToken)
+}
